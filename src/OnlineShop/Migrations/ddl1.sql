c4a6ac07a25187c57b6a1a9b4193d02f70ccd739
Flurin Brühwiler <47397416+FlurinBruehwiler@users.noreply.github.com>
2022-11-28 19:13:56 +0100

    feat(ddl): added initial ddl

---
+++ src/OnlineShop/Migrations/ddl1.sql
@@ -0,0 +1,50 @@
+CREATE TABLE category
+(
+    id   int AUTO_INCREMENT
+        PRIMARY KEY,
+    name varchar(40) NOT NULL
+);
+
+CREATE TABLE `order`
+(
+    id    int AUTO_INCREMENT
+        PRIMARY KEY,
+    email varchar(40)                          NOT NULL,
+    date  datetime DEFAULT CURRENT_TIMESTAMP() NOT NULL
+);
+
+CREATE TABLE product
+(
+    id          int AUTO_INCREMENT
+        PRIMARY KEY,
+    name        varchar(40)   NOT NULL,
+    description varchar(100)  NULL,
+    images      varchar(100)  NULL,
+    amount      int DEFAULT 0 NOT NULL,
+    price       int           NOT NULL
+);
+
+CREATE TABLE order_product
+(
+    `order`  int           NOT NULL,
+    product  int           NOT NULL,
+    quantity int DEFAULT 0 NOT NULL,
+    PRIMARY KEY (`order`, product),
+    CONSTRAINT order_product_order_null_fk
+        FOREIGN KEY (`order`) REFERENCES `order` (id),
+    CONSTRAINT order_product_product_null_fk
+        FOREIGN KEY (product) REFERENCES product (id)
+);
+
+CREATE TABLE product_category
+(
+    product  int NOT NULL,
+    category int NOT NULL,
+    PRIMARY KEY (product, category),
+    CONSTRAINT product_category_category_null_fk
+        FOREIGN KEY (category) REFERENCES category (id),
+    CONSTRAINT product_category_product_null_fk
+        FOREIGN KEY (product) REFERENCES product (id)
+);
+
+
